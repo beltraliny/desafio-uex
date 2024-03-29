125f2197e2b7fd5a8662870e1bfa7948465c1e73
aliny <beltraliny@hotmail.com>
2024-03-29 19:27:04 -0300

    Criação e validação de token JWT

---
+++ src/main/java/com/github/beltraliny/testeuex/security/TokenService.java
@@ -0,0 +1,61 @@
+package com.github.beltraliny.testeuex.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.github.beltraliny.testeuex.models.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.key}")
+    private String secretKey;
+
+    public String createToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+            Instant expirationTime = this.generateExpirationTime();
+
+            String token = JWT.create()
+                    .withIssuer("auth-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(expirationTime)
+                    .sign(algorithm);
+
+            return token;
+        } catch (JWTCreationException jwtCreationException) {
+            throw new RuntimeException("Authentication Error");
+        }
+    }
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secretKey);
+
+            String username = JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+            return username;
+        } catch (JWTVerificationException jwtVerificationException) {
+            return null;
+        }
+    }
+
+    private Instant generateExpirationTime() {
+        final long defaultExpirationTimeInHours = 1L;
+
+        return LocalDateTime.now()
+                .plusHours(defaultExpirationTimeInHours)
+                .toInstant(ZoneOffset.of("-03:00"));
+    }
+}
