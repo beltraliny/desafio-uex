d971336d13e98e94cb664ab8fc22455e53510fdc
aliny <beltraliny@hotmail.com>
2024-03-30 09:25:17 -0300

    Annotation Component em CustomDetailsService

--- src/main/java/com/github/beltraliny/testeuex/security/CustomUserDetailsService.java
+++ src/main/java/com/github/beltraliny/testeuex/security/CustomUserDetailsService.java
@@ -5,9 +5,11 @@ import com.github.beltraliny.testeuex.services.UserService;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserService userService;
